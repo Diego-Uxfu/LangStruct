afd33bae980939888056f23263803cbef3139d1b
Pete Thambundit <pthambundit@icloud.com>
2025-11-17 09:58:08 -0800

    added cfg

--- gui.py
+++ gui.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 
+from nltk import CFG
+from nltk.parse import EarleyChartParser
+from nltk.grammar import Nonterminal
+from random import choice
+
 
 # Create main window
 root = tk.Tk()
@@ -20,4 +25,29 @@ button.pack(pady=5)
 # Run the application
 root.mainloop()
 
+
+
+class SentenceGenerator:
+    def __init__(self):
+        self.grammar = CFG.fromstring("""
+            S -> NP VP
+            S -> NP VP PP
+            S -> NP VP NP
+            S -> NP VP NP PP
+
+            NP -> Det N
+            NP -> Det Adj N
+            NP -> Adj N
+            NP -> N
+            NP -> Pronoun
+
+            VP -> V
+            VP -> V NP
+            VP -> V PP
+            VP -> V NP PP
+
+            PP -> Prep NP
+
+        """)
+        self.parser = EarleyChartParser(self.grammar)
 
